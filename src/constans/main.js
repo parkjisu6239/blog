494805d6bffbaa9eeea157f4da42cb2613625abf
parkjisu6239 <parkjisu6239@gmail.com>
2022-07-04 22:04:04 +0900

    [ADD] add og static

--- src/constans/main.js
+++ src/constans/main.js
@@ -1,3 +1,8 @@
 export const github = "https://github.com/parkjisu6239";
 export const blogTitle = "Kate's blog";
 export const email = "parkjisu6239@gmail.com";
+
+export const og = {
+  title: "Kate's Blog",
+  description: "배우고 기록하는 프론트엔드 개발자",
+}
