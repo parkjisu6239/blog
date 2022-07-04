3534a26fe6515506f776cc4e572ed9b55dc8779c
parkjisu6239 <parkjisu6239@gmail.com>
2022-07-04 22:03:21 +0900

    [ADD] Add open graph

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -1,4 +1,5 @@
 import { postInfo } from "assets/posts/info";
+import { og } from "constans/main"
 
 export const isFileinCategory = (category, file) => {
   if (
@@ -9,4 +10,24 @@ export const isFileinCategory = (category, file) => {
     return true;
   }
   return false;
+};
+
+export const setMetaTags = ({
+  title = og.title,
+  description = og.description,
+}) => {
+  // set title
+  document
+    .querySelector("meta[property=\"og:title\"]")
+    .setAttribute("content", `${title}`);
+
+  // set description
+  document
+    .querySelector("meta[property=\"og:description\"]")
+    .setAttribute("content", description);
+
+  // set url
+  document
+    .querySelector("meta[property=\"og:url\"]")
+    .setAttribute("content", window.location.href);
 };
