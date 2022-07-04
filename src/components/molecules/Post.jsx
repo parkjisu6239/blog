606896d3f41f802a1c7490ad374280bc9fbf55fa
parkjisu6239 <parkjisu6239@gmail.com>
2022-07-04 22:04:56 +0900

    [MOD] Add og setting in post page

--- src/components/molecules/Post.jsx
+++ src/components/molecules/Post.jsx
@@ -1,10 +1,14 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { css } from "@emotion/css";
+
+import { mobile } from "styles/view";
+import { postInfo } from "assets/posts/info";
+import { setMetaTags } from "utils/misc"
 
 import Markdown from "components/atoms/Markdown";
 import Empty from "components/atoms/Empty";
 import GoTop from "components/atoms/GoTop";
-import { mobile } from "styles/view";
 
 const postCss = css`
   padding: 30px 0;
@@ -18,16 +22,26 @@ const Post = () => {
   const { category } = param;
   const { fileName } = param;
   let mdPath;
+  let curPost;
 
   try {
     if (!category) {
       mdPath = require("assets/posts/README.md");
     } else {
       mdPath = require(`assets/posts/${category}/${fileName}`);
+      curPost = postInfo[category].find(post => post.fileName === fileName)
     }
   } catch {
     return <Empty />;
   }
+
+  useEffect(() => {
+    if (!curPost) return
+    setMetaTags({
+      title: curPost.title,
+      description: curPost.desc,
+    })
+  }, [curPost])
 
   return (
     <article className={postCss}>
